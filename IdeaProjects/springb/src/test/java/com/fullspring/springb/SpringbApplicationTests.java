553fe437b77cddb669783a2821e7eee268b98fc1
abhi0111 <abhishek.jaiswal1994@gmail.com>
2019-12-31 03:32:41 +0530

    Initialized project

---
+++ IdeaProjects/springb/src/test/java/com/fullspring/springb/SpringbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullspring.springb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
